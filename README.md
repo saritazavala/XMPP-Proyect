6aa01e88e4a80661af58960c3038556b0cb59d9e
saritazavala <saritazg2010@gmail.com>
2021-08-11 02:24:41 -0600

    Starting some stuff

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # XMPP-Proyect
-El proyecto consiste en implementar un cliente que soporte el protocolo XMPP.
+## Objetivos 📌
+
+1. Apegarse a los estándares de un protocolo conocidoy abierto
+2. Comprender las bases de programacion asaincrona requeridas para apegarse a las necesidades de desarrollo.
+
+## Desarrollo 📋
+### *Características y limitaciones*
+El proyecto consiste en implementar un cliente que soporte el protocolo XMPP. A partir de ello debe de soportar como mínimo las siguientes características:
+
+1. Registrar una nueva cuenta en el servidor
+2. Iniciar sesión con una cuenta
+3. Cerrar sesión con una cuenta
+4. Eliminar la cuenta del servidor
---
+++ client.py
@@ -0,0 +1,40 @@
+import slixmpp
+import aiodns
+import asyncio 
+
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+
+class Client(slixmpp.ClientXMPP):
+    def __init__(self):
+        slixmpp.ClientXMPP.__init__(self, "zav18893@alumchat.xyz", "123")
+        self.to = "echobot@alumchat.xyz"
+        self.message = "Hola bot :v"
+        self.add_event_handler("session_start", self.start)
+        self.add_event_handler("message", self.message)
+
+    async def start(self, event):
+        print("Inicio de sesion")
+        self.send_presence()
+        await self.get_roster()
+        self.send_message(mto=self.to, mbody=self.message, mtype='chat')
+        self.disconnect()
+
+    def message(self, msg):
+        if msg['type'] in ('chat', 'normal'):
+            msg.reply("Thanks for sending\n%(body)s" % msg).send()
+
+
+xmpp = Client()
+xmpp.register_plugin('xep_0030')  # Service Discovery
+xmpp.register_plugin('xep_0004')  # Data Forms
+xmpp.register_plugin('xep_0060')  # PubSub
+xmpp.register_plugin('xep_0199')  # XMPP Ping
+
+# Connect to the XMPP server and start processing XMPP stanzas.
+xmpp.connect()
+xmpp.process(forever=False)
+print("1. Ver conectados")
+print(" 2. Mandar mensaje privado")
+print("3. Madar mensaje general")
+
